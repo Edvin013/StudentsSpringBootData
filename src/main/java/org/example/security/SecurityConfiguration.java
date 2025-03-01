64175039ac60aaec87088e84f2c47ca68b28ab1d
Edvin013 <Edvin10@bk.ru>
2025-03-01 10:08:42 +0300

    ---

--- src/main/java/org/example/security/SecurityConfiguration.java
+++ src/main/java/org/example/security/SecurityConfiguration.java
@@ -37,8 +37,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic();
-
-
     }
 
     @Bean
